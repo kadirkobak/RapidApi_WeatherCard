5aa9e072436bb803dd37e25f92ed2585be9f0f7f
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-30 16:35:14 +0300

    update

--- RapidApi_WeatherCard/Form1.Designer.cs
+++ RapidApi_WeatherCard/Form1.Designer.cs
@@ -28,7 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(247, 54);
+            label1.TabIndex = 0;
+            label1.Text = "İstanbul, TR";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.LemonChiffon;
+            pictureBox1.Location = new Point(140, 271);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 62);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(12, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(187, 41);
+            label2.TabIndex = 2;
+            label2.Text = "İstanbul, TR";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(21, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 31);
+            label3.TabIndex = 3;
+            label3.Text = "Today";
             // 
             // Form1
             // 
@@ -36,13 +83,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.CadetBlue;
             ClientSize = new Size(476, 466);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
     }
 }
